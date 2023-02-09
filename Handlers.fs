c9ac8700d8d61adda4fea81ffa346448c2ca74a4
Enrico Galassi <enrico.galassi.88@gmail.com>
2023-02-09 14:53:10 +0100

    Fix layout of the app

--- Handlers.fs
+++ Handlers.fs
@@ -6,53 +6,28 @@ open FsSpectre
 
 type Handlers =
 
-    static member private TodoPanel() =
-        panel {
-          expand
-          width 32
-          header_text "Todo"
-          content_renderable (
-            rows {
-              items_renderable [|
-                text { text "hello" }
-                text { text "hello" }
-              |]
-            }
-          )
-        }
-
     static member private NextEventsPanel() =
         panel {
-          expand
-          width 32
-          header_text "Next events"
-          content_renderable (
-            rows {
-              items_renderable [|
-                text { text "hello" }
-                text { text "hello" }
-              |]
-            }
-          )
+            expand
+            width 32
+            header_text "Next events"
+            content_renderable (rows { items_renderable [| text { text "hello" }; text { text "hello" } |] })
         }
 
     static member Handle(args: Arguments) =
         let now = DateTime.Now
+        let data = Storage.retrieve now
 
         match args with
         | Show ->
             grid {
                 number_of_columns 3
                 empty_row
-                row [| calendar { events [||] }; Handlers.TodoPanel (); Handlers.NextEventsPanel () |]
 
                 row
-                    [| text {
-                           text $"{now.Day} {now.DayOfWeek}"
-                           centered
-                       }
-                       panel { width 32 }
-                       panel { width 32 } |]
+                    [| Views.calendar now
+                       Views.todo data.ToDos now
+                       Views.nextEvents data.RecurringEvents data.Events now |]
 
                 empty_row
             }
